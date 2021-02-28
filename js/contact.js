f16c0d735d8919f1a33a7711f4178804b2fa940b
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-28 18:46:58 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -12,26 +12,45 @@ submit.onclick = function (event) {
   const subject = document.querySelector('#subject').value.trim();
   const email = document.querySelector('#email').value.trim();
   const address = document.querySelector('#address').value.trim();
+  let nameValidation = false;
+  let subjectValidation = false;
+  let emailValidation = false;
+  let addressValidation = false;
 
   if (testLength(name, 10)) {
     nameError.classList.add('hide');
+    nameValidation = true;
   } else {
     nameError.classList.remove('hide');
   }
   if (testLength(subject, 5)) {
     subjectError.classList.add('hide');
+    subjectValidation = true;
   } else {
     subjectError.classList.remove('hide');
   }
   if (validateEmail(email)) {
     emailError.classList.add('hide');
+    emailValidation = true;
   } else {
     emailError.classList.remove('hide');
   }
   if (testLength(address, 25)) {
     addressError.classList.add('hide');
+    addressValidation = true;
   } else {
     addressError.classList.remove('hide');
+  }
+
+  if (
+    nameValidation &&
+    subjectValidation &&
+    emailValidation &&
+    addressValidation
+  ) {
+    console.log('All good chief');
+  } else {
+    console.log('woops');
   }
 };
 
